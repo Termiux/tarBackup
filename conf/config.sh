fde9f47c21d974e7733d63a6654bf49f60202b61
Jorge Moreno <morenog.jorge@gmail.com>
2013-09-24 15:00:48 -0600

    Reworded some comments

--- conf/config.sh
+++ conf/config.sh
@@ -1,35 +1,22 @@
 
 ##################################################################################################################
 ##														##
-##   tarBackup shell script Tested on RHEL 5 and Debian 7							##
-##   Jorge A. Moreno morenog.jorge@gmail.com									##
-##   System Backup Bash Shell Script										##
+##		YOU NEED TO CHECK VALUES IN THIS FILE AND SET THEM TO MATCH YOUR SYSTEM				##
 ##														##
-##    You can use the script to backup your server using tar/gzip. Script can copy the backup files to a SMB	##
-##    share on another server, or just keep the backups locally if you wish. 					##
-##														##
-##    I've made some improvements, so its a little more smart now when something bad happens. 			##
-##    It also tries to inform you every time something unexpected happens so you always know what is going on.	##
-##														##
-##  How to use the script:											##
-##														##
-##	To use the script, you need to set a few values first. Check README File to know what you need to do	##
-##														##
-##	To use just drop the script in /etc/cron.daily/ or set it editing crontab. After that just forget it.	##
-##	If your cron is well configured you will be mailed of the script output. Anyway you will still be 	##
-##	mailed if something bad happens you will always be informed ;)						##
+##	This file contains all variables used by the script, everything is commented so you know what it is	##
+##	still if you dont know waht it does and cannot figure out with the comments then leave the default	##
 ##														##
 ##														##
 ##	>>> For detailed information about configuration and usage explanation consult the README file	<<<	##
 ##														##
 ##################################################################################################################
 
-##################################################
-##		Variables Declarations		##
-##################################################
 
 source '../lib/functions' 2>/dev/null
 
+##################################################
+##		Variables Declarations		##
+##################################################
 
 ###########################
 ## 	 Helpers	 ##	
